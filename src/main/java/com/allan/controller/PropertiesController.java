1ab3a3b9015d381d2ccfd8cfc3911d8b3dc6eaa6
柳学兵 <xuebing.liu@dhc.com.cn>
2021-05-11 14:51:11 +0800

    reindent tab with 4 spaces

--- src/main/java/com/allan/controller/PropertiesController.java
+++ src/main/java/com/allan/controller/PropertiesController.java
@@ -17,118 +17,118 @@ import javafx.util.Callback;
 import javafx.util.StringConverter;
 
 public class PropertiesController implements Initializable{
-	@FXML
-	private CheckBox  checkBoxPopUp;
-	@FXML
-	private RadioButton radioBtnPlayMusic, radioBtnSilence;
-	@FXML
-	private TextField textFieldMission;
-	@FXML
-	private TextArea textAreaNoticeWord;
-	@FXML
-	private DatePicker datePickerMission;
-	private Config config;
-	private ConfigDao configDao;
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		this.config = Cache.getCache().getConfigCache();
-		this.configDao = new ConfigDao();
-		//初始化弹窗
-		checkBoxPopUp.setSelected("Y".equals(config.getPopupOn()));
-		//初始化声音开关
-		if("Y".equals(config.getMusicOn())){
-			radioBtnPlayMusic.setSelected(true);
-		}
-			{
-			radioBtnSilence.setSelected(true);
-		}
-		//显示提醒文字
-		textAreaNoticeWord.setText(config.getNoticeText());
-		textFieldMission.setText(config.getMission());
-		//			设置早于当日的时间不能选择
-		final Callback<DatePicker, DateCell> dayCellFactory =
-				new Callback<DatePicker, DateCell>() {
-					@Override
-					public DateCell call(final DatePicker datePicker) {
-						return new DateCell() {
-							@Override
-							public void updateItem(LocalDate item, boolean empty) {
-								super.updateItem(item, empty);
-								if (item.isBefore(LocalDate.now())) {
-									setDisable(true);
-									setStyle("-fx-background-color: #ffc0cb;");
-								}
-							}
-						};
-					}
-		};
-		datePickerMission.setDayCellFactory(dayCellFactory);
-
-		//显示已设定的日期
-		LocalDate ld = LocalDate.parse(config.getDayCountdown());
-		datePickerMission.setValue(ld);
-	}
-
-	public void btnConfirmHandler(ActionEvent event) {
-		if(checkBoxPopUp.isSelected() && !"N".equals(config.getPopupOn())){
-			Config config = new Config();
-			config.setPopupOn(checkBoxPopUp.isSelected() ? "Y" : "N");
-			configDao.updateConfig(config);
+    @FXML
+    private CheckBox  checkBoxPopUp;
+    @FXML
+    private RadioButton radioBtnPlayMusic, radioBtnSilence;
+    @FXML
+    private TextField textFieldMission;
+    @FXML
+    private TextArea textAreaNoticeWord;
+    @FXML
+    private DatePicker datePickerMission;
+    private Config config;
+    private ConfigDao configDao;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.config = Cache.getCache().getConfigCache();
+        this.configDao = new ConfigDao();
+        //初始化弹窗
+        checkBoxPopUp.setSelected("Y".equals(config.getPopupOn()));
+        //初始化声音开关
+        if("Y".equals(config.getMusicOn())){
+            radioBtnPlayMusic.setSelected(true);
         }
-		//以下5个有且只有一个是true
-		Config configParam = new Config();
-		if(radioBtnPlayMusic.isSelected()){
-			configParam.setMusicOn("Y");
+        {
+            radioBtnSilence.setSelected(true);
         }
-		//silence
-		if(radioBtnSilence.isSelected()){
-		    configParam.setMute("Y");
+        //显示提醒文字
+        textAreaNoticeWord.setText(config.getNoticeText());
+        textFieldMission.setText(config.getMission());
+        //			设置早于当日的时间不能选择
+        final Callback<DatePicker, DateCell> dayCellFactory =
+                new Callback<DatePicker, DateCell>() {
+                    @Override
+                    public DateCell call(final DatePicker datePicker) {
+                        return new DateCell() {
+                            @Override
+                            public void updateItem(LocalDate item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (item.isBefore(LocalDate.now())) {
+                                    setDisable(true);
+                                    setStyle("-fx-background-color: #ffc0cb;");
+                                }
+                            }
+                        };
+                    }
+                };
+        datePickerMission.setDayCellFactory(dayCellFactory);
+
+        //显示已设定的日期
+        LocalDate ld = LocalDate.parse(config.getDayCountdown());
+        datePickerMission.setValue(ld);
+    }
+
+    public void btnConfirmHandler(ActionEvent event) {
+        if(checkBoxPopUp.isSelected() && !"N".equals(config.getPopupOn())){
+            Config config = new Config();
+            config.setPopupOn(checkBoxPopUp.isSelected() ? "Y" : "N");
+            configDao.updateConfig(config);
+        }
+        //以下5个有且只有一个是true
+        Config configParam = new Config();
+        if(radioBtnPlayMusic.isSelected()){
+            configParam.setMusicOn("Y");
+        }
+        //silence
+        if(radioBtnSilence.isSelected()){
+            configParam.setMute("Y");
         }
 
-		if (!textAreaNoticeWord.getText().equals(this.config.getNoticeText())){
-			configParam.setNoticeText(textAreaNoticeWord.getText());
-		}
+        if (!textAreaNoticeWord.getText().equals(this.config.getNoticeText())){
+            configParam.setNoticeText(textAreaNoticeWord.getText());
+        }
 
-		StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
-			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-			@Override
-			public String toString(LocalDate date) {
-				if (date != null) {
-					return dateFormatter.format(date);
-				} else {
-					return "";
-				}
-			}
-			@Override
-			public LocalDate fromString(String string) {
-				if (string != null && !string.isEmpty()) {
-					return LocalDate.parse(string, dateFormatter);
-				} else {
-					return null;
-				}
-			}
-		};
-		datePickerMission.setConverter(converter);
-		LocalDate localDate = datePickerMission.getValue();
-		configParam.setDayCountdown(localDate.toString());
-		configParam.setMission(textFieldMission.getText());
+        StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        };
+        datePickerMission.setConverter(converter);
+        LocalDate localDate = datePickerMission.getValue();
+        configParam.setDayCountdown(localDate.toString());
+        configParam.setMission(textFieldMission.getText());
 
-		//先把5个声音开始都置为N
-		Config configN = new Config();
-		configN.setMute("N");
-		configN.setMusicOn("N");
-		configDao.updateConfig(configN);
+        //先把5个声音开始都置为N
+        Config configN = new Config();
+        configN.setMute("N");
+        configN.setMusicOn("N");
+        configDao.updateConfig(configN);
 
-		configDao.updateConfig(configParam);
+        configDao.updateConfig(configParam);
 
-		//重新加载配置到内存
-		configDao.load();
+        //重新加载配置到内存
+        configDao.load();
 
-		Alert alert = new Alert(Alert.AlertType.INFORMATION);
-		alert.setTitle("提示");
-		alert.setHeaderText(" ");
-		alert.setContentText("保存成功");
-		alert.showAndWait();
-	}
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("提示");
+        alert.setHeaderText(" ");
+        alert.setContentText("保存成功");
+        alert.showAndWait();
+    }
 
 }
--- src/main/java/com/allan/dao/ConfigDao.java
+++ src/main/java/com/allan/dao/ConfigDao.java
@@ -13,77 +13,77 @@ import org.springframework.util.StringUtils;
 
 public class ConfigDao {
 
-	JdbcTemplate jdbcTemplate = SqliteHelper.getJdbcTemplate();
+    JdbcTemplate jdbcTemplate = SqliteHelper.getJdbcTemplate();
 
-	public void load(){
-		Cache.getCache().setConfigCache(this.findAll());
-	}
+    public void load(){
+        Cache.getCache().setConfigCache(this.findAll());
+    }
 
-	public Config findAll() {
-		List<Config> config = jdbcTemplate.query("select * from config", new RowMapper<Config>() {
-			@Override
-			public Config mapRow(ResultSet resultSet, int i) throws SQLException {
-				Config config = new Config();
-				config.setBreakTime(resultSet.getString("break_time"));
-				config.setPopupOn(resultSet.getString("popup_on"));
-				config.setPopupSize(resultSet.getString("popup_size"));
-				config.setNoticeText(resultSet.getString("notice_text"));
-				config.setMusicOn(resultSet.getString("music_on"));
-				config.setMute(resultSet.getString("mute"));
-				config.setDayCountdown(resultSet.getString("day_countdown"));
-				config.setMission(resultSet.getString("mission"));
-				return config;
-			}
-		});
-		return config.get(0);
-	}
+    public Config findAll() {
+        List<Config> config = jdbcTemplate.query("select * from config", new RowMapper<Config>() {
+            @Override
+            public Config mapRow(ResultSet resultSet, int i) throws SQLException {
+                Config config = new Config();
+                config.setBreakTime(resultSet.getString("break_time"));
+                config.setPopupOn(resultSet.getString("popup_on"));
+                config.setPopupSize(resultSet.getString("popup_size"));
+                config.setNoticeText(resultSet.getString("notice_text"));
+                config.setMusicOn(resultSet.getString("music_on"));
+                config.setMute(resultSet.getString("mute"));
+                config.setDayCountdown(resultSet.getString("day_countdown"));
+                config.setMission(resultSet.getString("mission"));
+                return config;
+            }
+        });
+        return config.get(0);
+    }
 
-	public void updateConfig(Config config) {
-		StringBuffer sql = new StringBuffer();
-		 sql.append("UPDATE config SET ");
-		if (!StringUtils.isEmpty(config.getBreakTime())) {
-			sql.append(" break_time = '");
-			sql.append(config.getBreakTime());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getPopupOn())) {
-			sql.append(" popup_on = '");
-			sql.append(config.getPopupOn());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getPopupSize())) {
-			sql.append(" popup_size = '");
-			sql.append(config.getPopupSize());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getNoticeText())) {
-			sql.append(" notice_text = '");
-			sql.append(config.getNoticeText());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getMusicOn())) {
-			sql.append(" music_on = '");
-			sql.append(config.getMusicOn());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getMute())) {
-			sql.append(" mute = '");
-			sql.append(config.getMute());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getDayCountdown())) {
-			sql.append(" day_countdown = '");
-			sql.append(config.getDayCountdown());
-			sql.append("', ");
-		}
-		if (!StringUtils.isEmpty(config.getMission())) {
-			sql.append(" mission = '");
-			sql.append(config.getMission());
-			sql.append("', ");
-		}
-		sql.deleteCharAt(sql.length()-2);//删掉最后的,
-		sql.append("WHERE id = '1'");
-		System.out.println("sql = " + sql);
-		jdbcTemplate.execute(sql.toString());
-	}
+    public void updateConfig(Config config) {
+        StringBuffer sql = new StringBuffer();
+        sql.append("UPDATE config SET ");
+        if (!StringUtils.isEmpty(config.getBreakTime())) {
+            sql.append(" break_time = '");
+            sql.append(config.getBreakTime());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getPopupOn())) {
+            sql.append(" popup_on = '");
+            sql.append(config.getPopupOn());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getPopupSize())) {
+            sql.append(" popup_size = '");
+            sql.append(config.getPopupSize());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getNoticeText())) {
+            sql.append(" notice_text = '");
+            sql.append(config.getNoticeText());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getMusicOn())) {
+            sql.append(" music_on = '");
+            sql.append(config.getMusicOn());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getMute())) {
+            sql.append(" mute = '");
+            sql.append(config.getMute());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getDayCountdown())) {
+            sql.append(" day_countdown = '");
+            sql.append(config.getDayCountdown());
+            sql.append("', ");
+        }
+        if (!StringUtils.isEmpty(config.getMission())) {
+            sql.append(" mission = '");
+            sql.append(config.getMission());
+            sql.append("', ");
+        }
+        sql.deleteCharAt(sql.length()-2);//删掉最后的,
+        sql.append("WHERE id = '1'");
+        System.out.println("sql = " + sql);
+        jdbcTemplate.execute(sql.toString());
+    }
 }
