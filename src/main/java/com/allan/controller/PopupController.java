0574558df68e0d762e757e25e9e97490c8d632e0
柳学兵 <xuebing.liu@dhc.com.cn>
2021-06-01 19:49:26 +0800

    增加弹窗倒计时显示

--- src/main/java/com/allan/controller/PopupController.java
+++ src/main/java/com/allan/controller/PopupController.java
@@ -2,6 +2,8 @@ package com.allan.controller;
 
 import com.allan.domain.Config;
 import com.allan.utils.Cache;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,8 +11,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class PopupController implements Initializable {
@@ -19,15 +23,21 @@ public class PopupController implements Initializable {
     private Button btnOk;
 
     @FXML
-    private Label lbMsg;
+    private Label lbMsg, lbCountdown;
 
     private Stage stage;
     private Config config;
+
+    private Timeline animation;
+    private String S = "";
+    private int tmp = 300;//单位 秒 5*60
+    DecimalFormat decimalFormat = new DecimalFormat("###################.###########");
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.config = Cache.getCache().getConfigCache();
         lbMsg.setText(config.getNoticeText());
+        clock();
     }
 
     public void setStage(Stage stage){
@@ -38,6 +48,24 @@ public class PopupController implements Initializable {
     public void btnOkHandler(ActionEvent event){
 //        System.out.println("btnOkHandler...");
         stage.close();
+    }
+
+    public void clock() {
+//        lbCountdown.setFont(javafx.scene.text.Font.font(20));
+        animation = new Timeline(new KeyFrame(Duration.millis(1000), e -> timelabel()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
+    }
+
+    public void timelabel() {
+        double minute = Math.floor(tmp / 60);
+        double second = tmp % 60;
+        tmp--;
+        S = decimalFormat.format(minute) + ":" + decimalFormat.format(second);
+        lbCountdown.setText(S);
+        if (tmp == 0) {
+            animation.stop();//停止计时
+        }
     }
 
 }
